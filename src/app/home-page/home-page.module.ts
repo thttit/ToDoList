b1ddc232f752f8f6c476fd1b8af6bc496a26153c
TRẦN HOÀNG THANH THY <103625040+thttit@users.noreply.github.com>
2023-03-23 14:55:59 +0700

    push

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     MatIconModule,
     MatListModule,
     HomePageRoutingModule,
-    FormsModule
+    FormsModule,
+    MatExpansionModule
   ]
 })
 export class HomePageModule { }
